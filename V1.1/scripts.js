6d142342350f03e870f7bfef9e0d7a4693b878a8
Bailey Gambrell <baileygambrell22@gmail.com>
2018-11-29 20:19:35 -0700

    Add basic V1 javascript functionality

--- V1.1/scripts.js
+++ V1.1/scripts.js
@@ -1,26 +1,26 @@
 
-let todoList = {
-    todos: [],
-
-    addTodo: function(todo) {
-        this.todos.push(todo);
-    },
-
-    displayTodos: function() {
-        console.log("My todos", this.todos);
-    },
-
-    
-
-
-}
-
 // It should have a space to store todos
-
-// It should have a way to display todos
+let todos = [];
 
 // It should have a way to add new todos
+function addTodo(todo) {
+    todos.push(todo);
+}
 
-// It should have a way to change a todo
+// It should have a way to display todos
+function displayTodos() {
+    console.log("My todos: " + todos);
+}
 
 // It should have a way to delete a todo
+function deleteTodo(i) {
+    todos.splice(i, 1);
+}
+
+// It should have a way to change a todo
+function changeTodo(text, position) {
+    let newTodo = text;
+    let i = position;
+
+    todos[i] = newTodo;
+}
