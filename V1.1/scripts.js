c48048fd74b431125bf56cb3c24751c9bc9726f5
Bailey Gambrell <baileygambrell22@gmail.com>
2018-11-30 10:53:17 -0700

    Convert everything into objects

--- V1.1/scripts.js
+++ V1.1/scripts.js
@@ -1,26 +1,36 @@
 
-// It should have a space to store todos
-let todos = [];
+let todoList = {
+    // It should have a space to store todos
+    todos: [],
 
-// It should have a way to add new todos
-function addTodo(todo) {
-    todos.push(todo);
+    // It should have a way to add new todos
+    addTodo: function(todo) {
+        todoList.todos.push(todo);
+    },
+
+    // It should have a way to display todos
+    displayTodos: function() {
+        console.log("My todos: " + todoList.todos);
+    },
+
+    // It should have a way to delete a todo
+    deleteTodo: function(i) {
+        todoList.todos.splice(i, 1);
+    },
+
+    // It should have a way to change a todo
+    changeTodo: function(text, position) {
+        let newTodo = text;
+        let oldPosition = position;
+
+        todoList.todos[oldPosition] = newTodo;
+    }
 }
 
-// It should have a way to display todos
-function displayTodos() {
-    console.log("My todos: " + todos);
-}
-
-// It should have a way to delete a todo
-function deleteTodo(i) {
-    todos.splice(i, 1);
-}
-
-// It should have a way to change a todo
-function changeTodo(text, position) {
-    let newTodo = text;
-    let i = position;
-
-    todos[i] = newTodo;
-}
+todoList.addTodo("new text");
+todoList.addTodo("second todo");
+todoList.displayTodos();
+todoList.deleteTodo(1);
+todoList.displayTodos();
+todoList.changeTodo("changed todo", 0);
+todoList.displayTodos();
