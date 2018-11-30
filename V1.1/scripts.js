9f539ce7b152f95a5da84f60fc5133d87ea17323
Bailey Gambrell <baileygambrell22@gmail.com>
2018-11-29 19:52:09 -0700

    Add initial javascript functions

---
+++ V1.1/scripts.js
@@ -0,0 +1,26 @@
+
+let todoList = {
+    todos: [],
+
+    addTodo: function(todo) {
+        this.todos.push(todo);
+    },
+
+    displayTodos: function() {
+        console.log("My todos", this.todos);
+    },
+
+    
+
+
+}
+
+// It should have a space to store todos
+
+// It should have a way to display todos
+
+// It should have a way to add new todos
+
+// It should have a way to change a todo
+
+// It should have a way to delete a todo
