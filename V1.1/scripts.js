433265ec4e64a9825b6e71ebbdc9c3b310045558
Bailey Gambrell <baileygambrell22@gmail.com>
2018-11-30 12:22:52 -0700

    Add properties to methods and add toggle functions

--- V1.1/scripts.js
+++ V1.1/scripts.js
@@ -4,33 +4,80 @@ let todoList = {
     todos: [],
 
     // It should have a way to add new todos
-    addTodo: function(todo) {
-        todoList.todos.push(todo);
+    addTodo: function(text) {
+        todoList.todos.push({
+            todoText: text,
+            completed: false
+        });
+        this.displayTodos();
     },
 
     // It should have a way to display todos
     displayTodos: function() {
-        console.log("My todos: " + todoList.todos);
+        let listLength = this.todos.length;
+        if (listLength === 0) {
+            console.log("Your todo list is empty!")
+        } else {
+            console.log("My todos: ")
+            for (let i = 0; i < listLength; i++) {
+                let completedDisplay;
+                let todoText = this.todos[i].todoText;
+
+                if (this.todos[i].completed === true) {
+                     completedDisplay = "(x)";
+                     console.log(completedDisplay + " " + todoText);
+                 } else {
+                     completedDisplay = "( )";
+                     console.log(completedDisplay + " " + todoText);
+                 }
+            }
+        }
     },
 
     // It should have a way to delete a todo
-    deleteTodo: function(i) {
-        todoList.todos.splice(i, 1);
+    deleteTodo: function(position) {
+        this.todos.splice(position, 1);
+        this.displayTodos();
     },
 
     // It should have a way to change a todo
     changeTodo: function(text, position) {
         let newTodo = text;
-        let oldPosition = position;
+        let todoPosition = position;
 
-        todoList.todos[oldPosition] = newTodo;
+        this.todos[todoPosition].todoText = newTodo;
+        this.displayTodos();
+    },
+
+    // Switch a todo's completed property
+    toggleCompleted: function(position) {
+        this.todos[position].completed = !this.todos[position].completed;
+        this.displayTodos();
+    },
+
+    // Switch the completed property of all todos to true or false
+    toggleAll: function() {
+        let listLength = this.todos.length;
+        let completedTodos = 0;
+        for (let i = 0; i < listLength; i++) {
+            if (this.todos[i].completed === true) {
+                completedTodos++;
+            }
+        }
+        if (completedTodos === listLength) {
+            for (let i = 0; i < listLength; i++) {
+                this.todos[i].completed = false;
+            }
+        } else {
+            for (let i = 0; i < listLength; i++) {
+                this.todos[i].completed = true;
+            }
+        }
+        this.displayTodos();
     }
+
 }
 
 todoList.addTodo("new text");
 todoList.addTodo("second todo");
-todoList.displayTodos();
-todoList.deleteTodo(1);
-todoList.displayTodos();
-todoList.changeTodo("changed todo", 0);
-todoList.displayTodos();
+todoList.addTodo("third todo");
