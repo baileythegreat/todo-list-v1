1a20969bc679f2770fc03dc8f29180584747013f
Bailey Gambrell <baileygambrell22@gmail.com>
2018-11-30 18:57:20 -0700

    Replaced for loops with forEach loops

--- V1.1/scripts.js
+++ V1.1/scripts.js
@@ -3,71 +3,66 @@ let todoList = {
     // It should have a space to store todos
     todos: [],
 
-    // It should have a way to add new todos
+    // Allows adding new todos
     addTodo: function(text) {
         todoList.todos.push({
             todoText: text,
             completed: false
         });
-        view.displayTodos();
     },
 
-    // It should have a way to delete a todo
+    // Allows deleting a todo
     deleteTodo: function(position) {
         this.todos.splice(position, 1);
-        view.displayTodos();
     },
 
-    // It should have a way to change a todo
+    // Allows editing the todo
     changeTodo: function(text, position) {
         let newTodo = text;
         let todoPosition = position;
 
         this.todos[todoPosition].todoText = newTodo;
-        view.displayTodos();
     },
 
     // Switch a todo's completed property
     toggleCompleted: function(position) {
         this.todos[position].completed = !this.todos[position].completed;
-        view.displayTodos();
     },
 
     // Switch the completed property of all todos to true or false
     toggleAll: function() {
         let listLength = this.todos.length;
         let completedTodos = 0;
-        for (let i = 0; i < listLength; i++) {
-            if (this.todos[i].completed === true) {
+        this.todos.forEach(function(todo) {
+            if (todo.completed === true) {
                 completedTodos++;
             }
-        }
+        })
         if (completedTodos === listLength) {
-            for (let i = 0; i < listLength; i++) {
-                this.todos[i].completed = false;
-            }
+            this.todos.forEach(function(todo) {
+                todo.completed = false;
+            })
         } else {
-            for (let i = 0; i < listLength; i++) {
-                this.todos[i].completed = true;
-            }
+            this.todos.forEach(function(todo) {
+                todo.completed = true;
+            })
         }
-        view.displayTodos();
+
     }
 }
 
 // Handles html interactions
 let handlers = {
 
-    displayTodos: function() {
-        todoList.displayTodos();
-    },
     toggleAll: function() {
         todoList.toggleAll();
+        view.displayTodos();
     },
     addTodo: function() {
         let todoText = $("#addTodoInput").val();
         todoList.addTodo(todoText);
         $("#addTodoInput").val("");
+        view.displayTodos();
     },
     changeTodo: function() {
         let text = $("#changeTodoText").val();
@@ -75,14 +70,17 @@ let handlers = {
         todoList.changeTodo(text, todoPosition);
         $("#changeTodoText").val("");
         $("#changeTodoPosition").val("");
+        view.displayTodos();
     },
     deleteTodo: function(todoPosition) {
         todoList.deleteTodo(todoPosition);
+        view.displayTodos();
     },
     toggleCompleted: function() {
         let todoPosition = $("#toggleCompletedPosition").val();
         todoList.toggleCompleted(todoPosition);
         $("#toggleCompletedPosition").val("");
+        view.displayTodos();
     }
 }
 
@@ -90,34 +88,32 @@ let view = {
 
     // Displays todos as an html list
     displayTodos: function() {
-
         let listLength = todoList.todos.length;
-        // Clears the todo list
+        // Resets the todo list
         let todosUl = document.querySelector(".todo-list");
         todosUl.innerHTML = "";
-
+        // Checks if the todos array is empty
         if (listLength === 0) {
             let noTodos = document.createElement("p");
             noTodos.innerHTML = "Your todo list is empty";
             todosUl.appendChild(noTodos);
         } else {
-            for (let i = 0; i < listLength; i++) {
+            todoList.todos.forEach(function(todo, position) {
                 let listItem = document.createElement("li");
-                let todoText = todoList.todos[i].todoText;
-                let deleteButton = this.createDeleteButton();
+                let deleteButton = view.createDeleteButton();
+                let todoText = todo.todoText;
 
-                if (todoList.todos[i].completed === true) {
+                // Indicates completed status
+                if (todo.completed === true) {
                      listItem.innerHTML = "(x) " + todoText;
-                     listItem.id = i;
-                     listItem.appendChild(deleteButton);
-                     todosUl.appendChild(listItem);
                  } else {
                      listItem.innerHTML = "( ) " + todoText;
-                     listItem.id = i;
-                     listItem.appendChild(deleteButton);
-                     todosUl.appendChild(listItem);
                  }
-            }
+                 // Creates list elements with an id and delete button, within the unordered list
+                 listItem.id = position;
+                 listItem.appendChild(deleteButton);
+                 todosUl.appendChild(listItem);
+            })
         }
     },
     // Creates a delete button
